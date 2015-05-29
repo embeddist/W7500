78640ff7dc43606392f0bf9b719385c127dbb31b
Soohwan Kim <suhwan@wiznet.co.kr>
2015-05-29 13:57:20 +0900

    Added WIZCHIP_READ_DMA / WIZCHIP_WRITE_DMA

--- W7500x_Library_Examples/Libraries/W7500x_stdPeriph_Driver/src/W7500x_wztoe.c
+++ W7500x_Library_Examples/Libraries/W7500x_stdPeriph_Driver/src/W7500x_wztoe.c
@@ -24,6 +24,167 @@ void WIZCHIP_WRITE(uint32_t Addr, uint8_t Data)
     *(volatile uint8_t *)(Addr) = Data;
     WIZCHIP_CRITICAL_EXIT();
 }
+#define _DEF_ACCESS_DMA_BUF_
+#ifdef _DEF_ACCESS_DMA_BUF_
+#include "W7500x_dma.h"
+#define MAX_TRANSNUM  1024
+void WIZCHIP_READ_DMA (uint32_t BaseAddr, uint16_t ptr, uint8_t* pBuf, uint16_t len)
+{
+    uint16_t i, loop;
+    uint32_t curr_state;
+    uint32_t size, dst;
+
+    dst =0;
+    size = 0;
+    //printf("@1- ptr: %X, len : %d, loop : %d\r\n",ptr, len, (len>>10)); 
+    loop = (len>>10);  
+    for(i=0; i<loop; i++)
+    {
+        size =0;
+        //printf("--- ptr+MAX_TRANSNUM : %X, ptr :%X \r\n",(uint32_t)(ptr+MAX_TRANSNUM), ptr); 
+        if((uint32_t)(ptr+MAX_TRANSNUM) > 0x10000 )  
+        {
+            size = 0x10000-ptr;
+            //printf("--- size : %d, ptr+size : %X \r\n",size , (ptr+size)); 
+            dma_memory_copy ( DMA_M2M5, (BaseAddr+(uint16_t)ptr), (unsigned int)&pBuf[dst], 0, (size));
+            do { /* Wait until PL230 DMA controller return to idle state */
+                curr_state = (DMA->DMA_STATUS >> 4)  & 0xF;
+            } while (curr_state!=0);  
+            //printf("--- dst : %d\r\n",dst); 
+            dma_memory_copy ( DMA_M2M5, (BaseAddr+(uint16_t)(0)), (unsigned int)&pBuf[dst+size], 0, (MAX_TRANSNUM-size));
+            do { /* Wait until PL230 DMA controller return to idle state */
+                curr_state = (DMA->DMA_STATUS >> 4)  & 0xF;
+            } while (curr_state!=0);    
+
+        }
+        else
+        {
+            //printf("--- dst : %d\r\n",dst); 
+            dma_memory_copy ( DMA_M2M5, (BaseAddr+(uint16_t)(ptr)), (unsigned int)&pBuf[dst], 0, MAX_TRANSNUM);
+            do { /* Wait until PL230 DMA controller return to idle state */
+                curr_state = (DMA->DMA_STATUS >> 4)  & 0xF;
+            } while (curr_state!=0);     
+
+        }
+
+        len -= MAX_TRANSNUM;
+        ptr += MAX_TRANSNUM;
+        dst += MAX_TRANSNUM;
+    }
+    if(len == 0) return;
+    //printf("@2- ptr: %X, dst: %d, len : %d\r\n",ptr, dst, len); 
+
+
+    size =0;
+    //printf("--- ptr+len : %X\r\n",(uint32_t)(ptr+len)); 
+    if( ((uint32_t)(ptr+len))>0x10000  )  			
+    {
+        size = 0x10000-ptr;
+        //printf("--- size : %d ptr: %X, len: %X \r\n",size, ptr, len); 
+        dma_memory_copy ( DMA_M2M5, (BaseAddr+(uint16_t)ptr), (unsigned int)&pBuf[dst], 0, size);
+        do { /* Wait until PL230 DMA controller return to idle state */
+            curr_state = (DMA->DMA_STATUS >> 4)  & 0xF;
+        } while (curr_state!=0);    
+        dst += size;
+        size = len - size;
+        //printf("--- dst : %d\r\n",dst); 
+        dma_memory_copy ( DMA_M2M5, (BaseAddr+(uint16_t)(0)), (unsigned int)&pBuf[dst], 0, size);
+        do { /* Wait until PL230 DMA controller return to idle state */
+            curr_state = (DMA->DMA_STATUS >> 4)  & 0xF;
+        } while (curr_state!=0);    				
+
+        len -= size;
+        ptr += size;
+        dst += size;						
+    }
+    else
+    {
+        dma_memory_copy ( DMA_M2M5, (BaseAddr+(uint16_t)(ptr)), (unsigned int)&pBuf[dst], 0, len);
+        do { /* Wait until PL230 DMA controller return to idle state */
+            curr_state = (DMA->DMA_STATUS >> 4)  & 0xF;
+        } while (curr_state!=0);     
+    }
+    //printf("@-3 ptr: %X, size: %d, len : %d\r\n",ptr, size, len); 
+
+
+}
+void WIZCHIP_WRITE_DMA(uint32_t BaseAddr, uint16_t ptr, uint8_t* pBuf, uint16_t len)
+{
+    uint16_t i, loop;
+    uint32_t curr_state;
+    uint32_t size, src;
+
+    src =0;
+    size = 0;
+    //printf("#1- ptr: %X, len : %d, loop : %d\r\n",ptr, len, (len>>10)); 
+    loop = (len>>10);  
+    for(i=0; i<loop; i++)
+    {
+        size =0;
+        //printf("--- ptr+MAX_TRANSNUM : %X, ptr :%X \r\n",(uint32_t)(ptr+MAX_TRANSNUM), ptr); 
+        if((uint32_t)(ptr+MAX_TRANSNUM) > 0x10000 )  
+        {
+            size = 0x10000-ptr;
+            //printf("--- size : %d, ptr+size : %X \r\n",size , (ptr+size)); 
+            dma_memory_copy ( DMA_M2M5, (unsigned int)&pBuf[src], (BaseAddr+(uint16_t)ptr), 0, size);
+            do { /* Wait until PL230 DMA controller return to idle state */
+                curr_state = (DMA->DMA_STATUS >> 4)  & 0xF;
+            } while (curr_state!=0);    
+            //printf("--- src : %d\r\n",src); 
+            dma_memory_copy ( DMA_M2M5, (unsigned int)&pBuf[src+size], (BaseAddr+(uint16_t)(0)), 0, (MAX_TRANSNUM-size));
+            do { /* Wait until PL230 DMA controller return to idle state */
+                curr_state = (DMA->DMA_STATUS >> 4)  & 0xF;
+            } while (curr_state!=0);    
+        }
+        else
+        {
+            dma_memory_copy ( DMA_M2M5, (unsigned int)&pBuf[src], (BaseAddr+(uint16_t)(ptr)), 0, MAX_TRANSNUM);
+            do { /* Wait until PL230 DMA controller return to idle state */
+                curr_state = (DMA->DMA_STATUS >> 4)  & 0xF;
+            } while (curr_state!=0);     
+        }
+
+
+        len -= MAX_TRANSNUM;
+		ptr += MAX_TRANSNUM;
+        src += MAX_TRANSNUM;
+    }
+    if(len == 0) return;
+    //printf("#2- ptr: %X, src: %d, len : %d\r\n",ptr, src, len); 
+
+
+    size =0;
+    //printf("--- ptr+len : %X\r\n",(uint32_t)(ptr+len)); 
+    if((uint32_t)(ptr+len) > 0x10000 )  
+    {
+        size = 0x10000-ptr;
+        //printf("--- size : %d ptr: %X, len: %X \r\n",size, ptr, len); 
+        dma_memory_copy ( DMA_M2M5, (unsigned int)&pBuf[src], (BaseAddr+(uint16_t)ptr), 0, size);
+        do { /* Wait until PL230 DMA controller return to idle state */
+            curr_state = (DMA->DMA_STATUS >> 4)  & 0xF;
+        } while (curr_state!=0);    
+        src += size;
+        size = len - size;
+        //printf("--- src : %d\r\n",src); 
+        dma_memory_copy ( DMA_M2M5, (unsigned int)&pBuf[src], (BaseAddr+(uint16_t)(0)), 0, size );
+        do { /* Wait until PL230 DMA controller return to idle state */
+            curr_state = (DMA->DMA_STATUS >> 4)  & 0xF;
+        } while (curr_state!=0);    
+
+        len -= size;
+        ptr += size;
+        src += size;						
+    }
+    else
+    {
+        dma_memory_copy ( DMA_M2M5, (unsigned int)&pBuf[src], (BaseAddr+(uint16_t)(ptr)), 0, len);
+        do { /* Wait until PL230 DMA controller return to idle state */
+            curr_state = (DMA->DMA_STATUS >> 4)  & 0xF;
+        } while (curr_state!=0);     
+    }
+    //printf("#-3 ptr: %X, size: %d, len : %d\r\n",ptr, size, len); 
+}
+#else
 void WIZCHIP_READ_BUF (uint32_t BaseAddr, uint32_t ptr, uint8_t* pBuf, uint16_t len)
 {
     uint16_t i = 0;
@@ -35,6 +196,7 @@ void WIZCHIP_READ_BUF (uint32_t BaseAddr, uint32_t ptr, uint8_t* pBuf, uint16_t 
     WIZCHIP_CRITICAL_EXIT();
 }
 
+
 void WIZCHIP_WRITE_BUF(uint32_t BaseAddr, uint32_t ptr, uint8_t* pBuf, uint16_t len)
 {
     uint16_t i = 0;
@@ -45,6 +207,8 @@ void WIZCHIP_WRITE_BUF(uint32_t BaseAddr, uint32_t ptr, uint8_t* pBuf, uint16_t 
 
     WIZCHIP_CRITICAL_EXIT();
 }
+#endif
+
 
 void wiz_send_data(uint8_t sn, uint8_t *wizdata, uint16_t len)
 {
@@ -54,7 +218,11 @@ void wiz_send_data(uint8_t sn, uint8_t *wizdata, uint16_t len)
     if(len == 0)  return;
     ptr = getSn_TX_WR(sn);
     sn_tx_base = (TXMEM_BASE) | ((sn&0x7)<<18);
+#ifdef _DEF_ACCESS_DMA_BUF_
+    WIZCHIP_WRITE_DMA (sn_tx_base, ptr, wizdata, len);
+#else
     WIZCHIP_WRITE_BUF(sn_tx_base, ptr, wizdata, len);
+#endif		
     ptr += len;
     setSn_TX_WR(sn,ptr);
 }
@@ -67,7 +235,11 @@ void wiz_recv_data(uint8_t sn, uint8_t *wizdata, uint16_t len)
     if(len == 0) return;
     ptr = getSn_RX_RD(sn);
     sn_rx_base = (RXMEM_BASE) | ((sn&0x7)<<18);
+#ifdef _DEF_ACCESS_DMA_BUF_
+    WIZCHIP_READ_DMA (sn_rx_base, ptr, wizdata, len);
+#else
     WIZCHIP_READ_BUF(sn_rx_base, ptr, wizdata, len);
+#endif	
     ptr += len;
     setSn_RX_RD(sn,ptr);
 }
